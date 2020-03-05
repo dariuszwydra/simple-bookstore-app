a7dc6cd7d926bf5ebdc1cca5947565dce4ac7b6b
dariuszwydra <dariusz.wydra@hotmail.com>
2020-03-05 12:31:04 +0100

    Initial commit. Created spring boot project and configured the application.properties file.

---
+++ simple-bookstore-app/src/test/java/com/dariuszwydra/bookstore/OnlineBookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dariuszwydra.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
